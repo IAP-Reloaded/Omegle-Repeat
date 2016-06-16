731ae35c2d1793679f1abd7dc2140b474247ff7c
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-15 20:18:59 -0400

    bump version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Omegle Repeat
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        http*://www.omegle.com/
