1b4abe61c8cb1ecee93bf478093456ab1a93280e
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-15 20:22:52 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Omegle Repeat
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        http*://www.omegle.com/
 // @grant        none
 // ==/UserScript==
 
-$('textarea.chatmsg').val("hi");
-$('button.sendbtn').click();
+setInterval(function(){
+    $('textarea.chatmsg').val("hi");
+    $('button.sendbtn').click();
+}, 2500);
