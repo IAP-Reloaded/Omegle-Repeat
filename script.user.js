96269b829b44181707fa52f48073e5081ee48fbd
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-15 20:18:49 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        http*://*omegle.com/
+// @match        http*://www.omegle.com/
 // @grant        none
 // ==/UserScript==
 
