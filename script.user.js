3525addd30b4327b18ecb6d12d0209a200ebb94d
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-15 20:12:37 -0400

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Omegle Repeat
+// @namespace    http://simpcraft.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       IAP-Reloaded on GitHub
+// @match        http://omegle.com/
+// @grant        none
+// ==/UserScript==
+
+$('textarea.chatmsg').val("hi");
+$('button.sendbtn').click();
